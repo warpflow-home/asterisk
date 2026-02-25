75ff41c5e3ec9892b862587a7bb5f82006a3bcb5
Noname <you@example.com>
2026-02-25 20:58:55 +0900

    CUPSプリンタのホスト名をlocalhostからcupsに変更

--- script/printfax-dummy.sh
+++ script/printfax-dummy.sh
@@ -10,7 +10,7 @@ PDFFILE="${FAXFILE%.*}.pdf"
 
 curl -sL "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" -o "$PDFFILE"
 tiff2pdf -o "$PDFFILE" -p A4 -F "$FAXFILE"
-lp -h localhost:631 -d Canon_G3060 -o media=A4 -o fit-to-page "$PDFFILE"
+lp -h cups:631 -d Canon_G3060 -o media=A4 -o fit-to-page "$PDFFILE"
 
 if [ $? -eq 0 ]; then
     curl -H "Title: FAX印刷成功" \
