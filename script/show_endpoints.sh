f0346c9e014c60d9570b2128c674575134d984e5
Noname <you@example.com>
2026-03-07 21:56:07 +0900

    refactor: Improve awk parsing in show_endpoints.sh to handle leading spaces and skip header lines

--- script/show_endpoints.sh
+++ script/show_endpoints.sh
@@ -6,11 +6,15 @@ DATETIME=$(TZ='Asia/Tokyo' date '+%Y-%m-%d %H:%M:%S')
 # Asteriskの情報を取得
 RAW_DATA=$(/usr/sbin/asterisk -rx "pjsip show endpoints")
 
-# awkで整形
+# awkで整形（行頭のスペースを許容し、ヘッダーを無視するよう改良）
 FORMATTED_INFO=$(echo "$RAW_DATA" | awk '
-BEGIN { FS=" "; prev_name=""; prev_ip=""; found=0 }
+BEGIN { prev_name=""; prev_ip=""; found=0 }
 
-/^Endpoint:/ {
+# "Endpoint:" を含む行（行頭のスペースも許可）
+/^[ \t]*Endpoint:/ {
+    # "<Endpoint..." のようなヘッダー行はスキップ
+    if ($2 ~ /^</) next;
+    
     if (prev_name!="") {
         printf "%s: %s\n", prev_name, (prev_ip?prev_ip:"-")
         found=1
@@ -18,18 +22,23 @@ BEGIN { FS=" "; prev_name=""; prev_ip=""; found=0 }
     prev_name=$2; prev_ip=""; next
 }
 
-/^Contact:/ {
+/^[ \t]*Contact:/ {
+    if ($2 ~ /^</) next;
     if ($2 ~ /@/) { match($2, /@[^:;]+/); prev_ip=substr($2, RSTART+1, RLENGTH-1) }
     else if ($2 ~ /^[0-9]/) prev_ip=$2
     next
 }
 
-/^Match:/ { split($2,m,"/"); prev_ip=m[1]; next }
+/^[ \t]*Match:/ {
+    if ($2 ~ /^</) next;
+    split($2,m,"/"); prev_ip=m[1]; next
+}
 
 END {
     if (prev_name!="") { printf "%s: %s\n", prev_name, (prev_ip?prev_ip:"-"); found=1 }
 }')
 
+# もし整形結果が空なら、元のデータをそのまま入れる
 if [ -z "$FORMATTED_INFO" ]; then
     CONTENT="解析エラーまたはデータなし:\n${RAW_DATA}"
 else
